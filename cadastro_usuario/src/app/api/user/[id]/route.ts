d82c17514b0853792a993d6ef11e6284df94a2ca
JuniorBandeira7 <juliobandeira77@gmail.com>
2024-11-02 10:56:33 -0300

    refactor: tirando logica a mais do try e adicionando permisoes

--- cadastro_usuario/src/app/api/user/[id]/route.ts
+++ cadastro_usuario/src/app/api/user/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "@db/db"
 import bcrypt from 'bcryptjs'
+import { Prisma } from "@prisma/client";
 
 // helpers
 const getUserByToken = require('../../../helpers/get-user-by-token')
@@ -84,70 +85,71 @@ export async function PATCH(req: Request, { params }: { params: { id: string}}){
     const { id } = await params;
 
     const { name, email, senha } = await req.json()
+
+    // Validações
+    if(!name){
+        return NextResponse.json(
+            {
+                message: 'o nome é obrigatório'
+            },
+            {
+                status: 422
+            }
+        )
+    }
+    if(!email){
+        return NextResponse.json(
+            {
+                message: 'O email é obrigatório'
+            },
+            {
+                status: 422
+            }
+        )
+    }
+
+    // Busca o token e depois busca o usuario pelo token
+    const token = getToken(req)
+    const tokenUser = await getUserByToken(token)
+
+    // Verifica se o id passado é o mesmo do usuario do token ou se é admin
+    if(parseInt(id, 10) !== tokenUser.id && tokenUser.permissao !== true){
+        return NextResponse.json(
+            {
+                message: "Acesso negado!"
+            },
+            {
+                status: 422
+            }
+        )
+    }
+    // Verificar se usuario já existe
+    const userExists = await prisma.user.findUnique({
+        where: {
+        email: email,
+        },
+    });
+    // verifica se quem está fazendo a mudança é admin pois admin pode alterar outros emails
+    if(userExists && tokenUser.email !== email && tokenUser.permissao !== true){
+        return NextResponse.json(
+            {
+                message: 'Email já cadastrado'
+            },
+            {
+                status: 422
+            }
+        )
+    }
+    // Pego a senha já criptografada do usuario caso ele não decida mudar de senha
+    let passwordHash = tokenUser.senha
+
+    if(senha){
+        // Criar senha com criptografia
+        const salt = await bcrypt.genSalt(12)
+        passwordHash = await bcrypt.hash(senha, salt)
+    }
     
     try {
-        // Busca o token e depois busca o usuario pelo token
-        const token = getToken(req)
-        const tokenUser = await getUserByToken(token)
-
-        // Verifica se o id passado é o mesmo do usuario do token ou se é admin
-        if(parseInt(id, 10) !== tokenUser.id && tokenUser.permissao !== true){
-            return NextResponse.json(
-                {
-                    message: "Acesso negado!"
-                },
-                {
-                    status: 422
-                }
-            )
-        }
-        // Validações
-        if(!name){
-            return NextResponse.json(
-                {
-                    message: 'o nome é obrigatório'
-                },
-                {
-                    status: 422
-                }
-            )
-        }
-        if(!email){
-            return NextResponse.json(
-                {
-                    message: 'O email é obrigatório'
-                },
-                {
-                    status: 422
-                }
-            )
-        }
-        // Verificar se usuario já existe
-        const userExists = await prisma.user.findUnique({
-            where: {
-            email: email,
-            },
-        });
-
-        if(userExists && tokenUser.email !== email){
-            return NextResponse.json(
-                {
-                    message: 'Email já cadastrado'
-                },
-                {
-                    status: 422
-                }
-            )
-        }
-        // Pego a senha já criptografada do usuario caso ele não decida mudar de senha
-        let passwordHash = tokenUser.senha
-
-        if(senha){
-            // Criar senha com criptografia
-            const salt = await bcrypt.genSalt(12)
-            passwordHash = await bcrypt.hash(senha, salt)
-        }
-        
         const user = await prisma.user.update({
             where:{
                 id: parseInt(id, 10)
@@ -161,6 +163,18 @@ export async function PATCH(req: Request, { params }: { params: { id: string}}){
 
         return NextResponse.json({ message: "OK", user})
     } catch (error) {
+        // É necessária essa verificação pois admin pode alterar outros emails
+        // Se o admin mudar o email de alguem para algum que já existe não iria entrar na verificação anterior
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002"){
+            return NextResponse.json(
+                {
+                    message: "Email já cadastrado!",
+                },
+                {
+                    status: 422,
+                }
+            )
+        }
         return NextResponse.json(
             {
                 message: "Error",
