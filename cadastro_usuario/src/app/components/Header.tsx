2a6ebbeb763510c644087126a4cc3ba8e45db23c
JuniorBandeira7 <juliobandeira77@gmail.com>
2024-11-05 17:24:25 -0300

    feat: header leva para a pagina de usuarios e editar

--- cadastro_usuario/src/app/components/Header.tsx
+++ cadastro_usuario/src/app/components/Header.tsx
@@ -1,11 +1,35 @@
-"use client";
+"use client"
 
-import Link from 'next/link';
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+interface User {
+    id: string
+    name: string
+    email: string
+    permissao: boolean
+}
 
 export default function Header() {
+    const [user, setUser] = useState<User | null>(null)
+
     const handleLogout = () => {
-        
-    };
+
+    }
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const token = localStorage.getItem("token")
+            if (!token) {
+                alert("Token não encontrado. Você precisa estar logado.")
+                return
+            }
+
+            const decodedToken = JSON.parse(atob(token.split('.')[1])) // // Decodifica o token e pega as informações do usuario
+            setUser(decodedToken)
+        }
+        fetchUsers()
+    }, [])
 
     return (
         <header className="flex items-center justify-between p-4 h-16 bg-gray-800 text-white">
@@ -15,10 +39,10 @@ export default function Header() {
                 </Link>
             </div>
             <nav className="flex items-center gap-6">
-                <Link href="/editar" className="hover:text-gray-400 font-semibold">Editar</Link>
-                <Link href="/" className="hover:text-gray-400 font-semibold">Usuários</Link>
+                <Link href={`/editar?id=${user?.id}`} className="hover:text-gray-400 font-semibold">Editar</Link>
+                <Link href="/usuarios" className="hover:text-gray-400 font-semibold">Usuários</Link>
                 <button onClick={handleLogout} className="hover:text-gray-400 font-semibold focus:outline-none"> Deslogar </button>
             </nav>
         </header>
-    );
+    )
 }
