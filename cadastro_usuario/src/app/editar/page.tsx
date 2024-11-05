e55b29907a3a0a94da7bae00bf680373c7df6db7
JuniorBandeira7 <juliobandeira77@gmail.com>
2024-11-05 17:25:40 -0300

    fix: passando "Authorization" na requisição á api

--- cadastro_usuario/src/app/editar/page.tsx
+++ cadastro_usuario/src/app/editar/page.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation"
 import Button from "../components/Button"
+import Header from "../components/Header";
 
 export default function EditUserPage() {
   const [name, setName] = useState("")
@@ -12,22 +13,22 @@ export default function EditUserPage() {
   const router = useRouter()
   const searchParams = useSearchParams();
   const id = parseInt(searchParams.get("id") ?? "0", 10)
+  const token = localStorage.getItem("token")
 
   useEffect(() => {
     async function validateToken() {
-      const token = localStorage.getItem("token")
       const tokenResponse = await fetch(`/api/auth/validate-token/${id}`, {
         method: "GET",
         headers: {
-            "Authorization": `Bearer ${token}`,
+          "Authorization": `Bearer ${token}`
         },
-    })
-        if (!tokenResponse.ok) {
-            router.push('/') // Redireciona para a página inicial se o token não for válido
-            return
-        } 
+      })
+      if (!tokenResponse.ok) {
+        router.push('/')
+        return
       }
-      validateToken()
+    }
+    validateToken()
 
     // Função para buscar os dados do usuário na rota GET e preenchê-los no formulário
     async function fetchUser() {
@@ -51,13 +52,16 @@ export default function EditUserPage() {
     try {
       const response = await fetch(`/api/user/${id}`, {
         method: "PATCH",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
         body: JSON.stringify({ name, email, senha }),
       })
       const data = await response.json()
       if (response.ok) {
         console.log("Usuário atualizado com sucesso")
-        router.push("/") // Redireciona para a página de tarefas
+        router.push("/")
       } else {
         console.log(data.message)
       }
@@ -70,11 +74,15 @@ export default function EditUserPage() {
     try {
       const response = await fetch(`/api/user/${id}`, {
         method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
       })
       const data = await response.json()
       if (response.ok) {
         console.log("Usuário excluído com sucesso")
-        router.push("/home") // Redireciona para a página de tarefas
+        router.push("/")
       } else {
         console.log(data.message)
       }
@@ -84,37 +92,40 @@ export default function EditUserPage() {
   }
 
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <div className="flex flex-col items-center justify-center bg-gray-800 p-8 rounded-lg w-96">
-        <h1 className="text-white text-3xl mb-6">Editar Usuário</h1>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Nome"
-          className="mb-4 p-2 rounded bg-gray-700 text-white w-full"
-        />
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          className="mb-4 p-2 rounded bg-gray-700 text-white w-full"
-        />
-        <input
-          type="password"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-          placeholder="Senha"
-          className="mb-4 p-2 rounded bg-gray-700 text-white w-full"
-        />
-        <Button onClick={handleUpdate} className="mb-2 w-full bg-blue-600 text-white">
-          Atualizar
-        </Button>
-        <Button onClick={handleDelete} className="w-full bg-red-600 text-white">
-          Excluir
-        </Button>
+    <>
+      <Header />
+      <div className="flex items-center justify-center h-screen bg-black">
+        <div className="flex flex-col items-center justify-center bg-gray-800 p-8 rounded-lg w-96">
+          <h1 className="text-white text-3xl mb-6">Editar Usuário</h1>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Nome"
+            className="mb-4 p-2 rounded bg-gray-700 text-white w-full cursor-text"
+          />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="mb-4 p-2 rounded bg-gray-700 text-white w-full cursor-text"
+          />
+          <input
+            type="password"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            placeholder="Senha"
+            className="mb-4 p-2 rounded bg-gray-700 text-white w-full cursor-text"
+          />
+          <Button onClick={handleUpdate} className="mb-2 w-full bg-blue-600 text-white">
+            Atualizar
+          </Button>
+          <Button onClick={handleDelete} className="w-full bg-red-600 text-white">
+            Excluir
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
