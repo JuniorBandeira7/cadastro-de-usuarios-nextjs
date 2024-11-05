c7acaeb841a0dae6171143ce8ac3547cfa5a073b
JuniorBandeira7 <juliobandeira77@gmail.com>
2024-11-05 17:23:48 -0300

    refactor: onClick opcional

--- cadastro_usuario/src/app/components/Button.tsx
+++ cadastro_usuario/src/app/components/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 interface ButtonProps {
-    onClick: () => void 
+    onClick?: () => void 
     children: React.ReactNode 
     className?: string
 }
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps> = ({ onClick, children, className }) => {
     return (
         <button
             onClick={onClick}
-            className={`bg-blue-500 text-white font-bold py-2 px-4 rounded ${className}`}
+            className={`bg-blue-500 text-white font-bold py-2 px-4 rounded cursor-pointer ${className}`}
         >
             {children}
         </button>
