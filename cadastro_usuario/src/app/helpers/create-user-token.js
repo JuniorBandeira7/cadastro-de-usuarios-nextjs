ce66c13ab6326823ad8a17d0e0bbf38588b730af
JuniorBandeira7 <juliobandeira77@gmail.com>
2024-11-05 17:26:09 -0300

    refactor: ao criar token passa mais informações do usuario

--- cadastro_usuario/src/app/helpers/create-user-token.js
+++ cadastro_usuario/src/app/helpers/create-user-token.js
@@ -1,14 +1,8 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
 const createUserToken = async (user) => {
-    const token = jwt.sign(
-        {
-            name: user.name,
-            id: user.id 
-        },
-        "secret"
-    );
+    const token = jwt.sign({ id: user.id, nome: user.nome, permissao: user.permissao }, 'secret', { expiresIn: '48h' })
     return token
-};
+}
 
-module.exports = createUserToken;
+module.exports = createUserToken
